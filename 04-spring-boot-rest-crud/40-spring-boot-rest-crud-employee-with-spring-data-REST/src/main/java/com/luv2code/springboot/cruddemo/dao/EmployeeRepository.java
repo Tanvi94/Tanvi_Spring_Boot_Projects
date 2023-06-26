fc1624d38aae34f694de506e5765d87434a4cb16
Tanvi94 <madhekartanvi9@gmail.com>
2023-06-26 21:51:26 +0200

    Spring Boot REST Configurations

--- 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-REST/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-REST/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,12 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 //employee and primary key type
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
 
     //that's it .. no need to write any code LOL!
